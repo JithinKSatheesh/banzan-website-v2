bc3812839170324db9da46a7456c7dbfae4bf81d
Jithin K Satheesh <jithin@banzan.co>
2023-10-30 23:03:37 +0530

    igdc update

--- src/Components/Home/Promo.js
+++ src/Components/Home/Promo.js
@@ -1,7 +1,8 @@
 import React from 'react'
 
 import _promo from './top23.jpg'
-import _promo2 from './Dubai_Creative_Insta.jpeg'
+// import _promo2 from './Dubai_Creative_Insta.jpeg'
+import _promo3 from './IGDC.jpeg'
 import Partners from './Partners'
 
 export default function Promo(props) {
@@ -12,7 +13,7 @@ export default function Promo(props) {
             <div className="">
                 <div className="flex flex-wrap justify-center mt-[100px]">
                     <div className="mx-auto_ max-w-[400px]  mt-[20px] border-[20px] rounded-xl border-[#c42f48] drop-shadow-xl m-[10px]">
-                        <img src={_promo2} alt=""  className='w-full'/>
+                        <img src={_promo3} alt=""  className='w-full'/>
                     </div>
                     <div className=" flex flex-col justify-center items-center mx-auto_ max-w-[400px]  mt-[20px] border-[20px] rounded-xl border-[#c42f48] drop-shadow-xl m-[10px]">
                         <img src={_promo} alt=""  className='w-full '/>
